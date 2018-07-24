7f80e0a0f52e998d1699ee15d4f2d3f350fd2354
Jeffrey Polasz <jeff_polasz@hotmail.com>
2018-07-23 21:38:26 -0400

    Generating random numbers

---
+++ Source/TestingGrounds/Terrain/Tile.h
@@ -0,0 +1,31 @@
+// Copyright 2018 Jeffrey Polasz. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Tile.generated.h"
+
+UCLASS()
+class TESTINGGROUNDS_API ATile : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ATile();
+
+	UFUNCTION(BlueprintCallable, Category = "Weapon")
+	void PlaceActors();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	
+	
+};
