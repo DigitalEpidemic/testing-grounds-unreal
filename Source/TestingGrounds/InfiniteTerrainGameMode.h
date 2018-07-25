587395f49a4f5f1bd2e178212878f41b26fd1d7e
Jeffrey Polasz <jeff_polasz@hotmail.com>
2018-07-25 16:22:52 -0400

    Find actors of type from C++

---
+++ Source/TestingGrounds/InfiniteTerrainGameMode.h
@@ -0,0 +1,24 @@
+// Copyright 2018 Jeffrey Polasz. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TestingGroundsGameMode.h"
+#include "InfiniteTerrainGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTINGGROUNDS_API AInfiniteTerrainGameMode : public ATestingGroundsGameMode
+{
+	GENERATED_BODY()
+	
+public:
+	UFUNCTION(BlueprintCallable, Category = "Bounds Pool")
+	void PopulateBoundsVolumePool();
+	
+private:
+	void AddToPool(class ANavMeshBoundsVolume *VolumeToAdd);
+
+};
